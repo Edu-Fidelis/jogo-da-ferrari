775406a58c853227e426d2159f57c11ea7020efa
Edu-Fidelis <68570741+Edu-Fidelis@users.noreply.github.com>
2024-05-27 22:28:35 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('my-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/styles.css',
+        '/app.js',
+        '/caminho/para/seu/icone-192x192.png',
+        '/caminho/para/seu/icone-512x512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
